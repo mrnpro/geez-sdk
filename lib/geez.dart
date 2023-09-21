be550e816008219b5d5b11cdbd93d9a141fcb22b
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2023-09-21 07:58:58 -0700

    add documentation

--- lib/geez.dart
+++ lib/geez.dart
@@ -1,7 +1,31 @@
+/// The `geez` library provides utility extensions and functions for working with Geez numbers.
+///
+/// Geez is the script used for writing the Ge'ez language, which is an ancient South Semitic
+/// language that is still used liturgically by the Ethiopian Orthodox Tewahedo Church and
+/// Eritrean Orthodox Tewahdo Church. The Geez script has its own numeric system.
+///
+/// This library exports two main components:
+/// - [GeezToNumberConvertor]: A class that provides a utility method to convert Geez numbers to
+///   their corresponding numeric values.
+/// - [NumberToGeezConvertor]: An extension on `int` objects that allows converting numbers to
+///   Geez numbers represented as strings.
+///
+/// Example usage:
+/// ```dart
+/// import 'geez/geez.dart';
+///
+/// void main() {
+///   // Convert Geez numbers to their numeric values
+///   int numericValue ='፪፻'.toArabic(); // Converts to 200
+///
+///   // Convert numbers to Geez numbers
+///   int number = 200;
+///   String geezNumber = number.toGeez(); // Converts to '፪፻'
+/// }
+/// ```
+///
+
 library geez;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export './src/geez_to_number.dart';
+export './src/number_to_geez.dart';
--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,4 +1,33 @@
-final geezNumbers = {0: '', ...oneToNine, ...tenToNinety, ...hundredTo1M};
+/// The `geezNumbers` constant provides a mapping between numeric values and their corresponding
+/// Geez representations.
+///
+/// Geez is the script used for writing the Ge'ez language, which is an ancient South Semitic
+/// language that is still used liturgically by the Ethiopian Orthodox Tewahedo Church and
+/// Eritrean Orthodox Tewahdo Church. The Geez script has its own numeric system.
+///
+/// The `geezNumbers` constant combines three sub-constants: `oneToNine`, `tenToNinety`, and
+/// `hundredTo1M`, which represent the Geez representations for numbers from 1 to 9, numbers
+/// in the tens place (10, 20, 30, etc.), and numbers in the hundreds and higher places (100, 1000,
+/// 10000, etc.), respectively.
+///
+/// Example usage:
+/// ```dart
+/// import 'geez/constants.dart';
+///
+/// void main() {
+///   print(geezNumbers[5]); // Outputs '፭'
+///   print(geezNumbers[40]); // Outputs '፵'
+///   print(geezNumbers[1000]); // Outputs '፲፻'
+/// }
+/// ```
+final geezNumbers = {
+  0: '', // Zero is represented as an empty string in Geez.
+  ...oneToNine,
+  ...tenToNinety,
+  ...hundredTo1M,
+};
+
+/// The `oneToNine` constant provides the Geez representations for numbers from 1 to 9.
 final oneToNine = {
   1: '፩',
   2: '፪',
@@ -10,6 +39,9 @@ final oneToNine = {
   8: '፰',
   9: '፱',
 };
+
+/// The `tenToNinety` constant provides the Geez representations for numbers in the tens place
+/// (10, 20, 30, etc.).
 final tenToNinety = {
   10: '፲',
   20: '፳',
@@ -21,10 +53,13 @@ final tenToNinety = {
   80: '፹',
   90: '፺',
 };
+
+/// The `hundredTo1M` constant provides the Geez representations for numbers in the hundreds and
+/// higher places (100, 1000, 10000, etc.).
 final hundredTo1M = {
   100: '፻',
   1000: '፲፻',
   10000: '፼',
   100000: '፲፼',
-  1000000: '፻፼'
+  1000000: '፻፼',
 };
