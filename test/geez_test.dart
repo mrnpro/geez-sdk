9e13d303bc19d90aa3f7397014b5aee86a15bbc4
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2023-09-21 08:10:48 -0700

    add unit test and mock

--- test/geez_test.dart
+++ test/geez_test.dart
@@ -1,12 +1,40 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:geez/geez.dart';
 
+import 'mock_geez_data.dart';
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  // Generate a list of numbers from 1 to 1999
+  List<int> numbersList = List<int>.generate(1999, (index) => index + 1);
+
+  group(
+    'Geez Conversion Tests',
+    () => {
+      test('Convert a number to Geez - Specific Number', () {
+        // Convert a specific number to Geez representation
+        String geezRepresentation = 134.toGeez();
+        expect(geezRepresentation, '፻፴፬');
+      }),
+      test('Convert a number to Geez - Multiple Numbers', () {
+        // Convert multiple numbers to Geez representation
+        for (var i = 0; i < numbersList.length; i++) {
+          String convertedToGeez = numbersList[i].toGeez();
+
+          expect(convertedToGeez, mockGeezNumbers[i]);
+        }
+      }),
+      test('Convert Geez to Number - Specific Number', () {
+        // Convert a specific Geez representation to number
+        num number = '፻፴፬'.toArabic();
+        expect(number, 134);
+      }),
+      test('Convert Geez to Number - Multiple Numbers', () {
+        // Convert multiple Geez representations to numbers
+        for (var i = 0; i < numbersList.length; i++) {
+          num convertedFromGeez = mockGeezNumbers[i].toArabic();
+          expect(convertedFromGeez, numbersList[i]);
+        }
+      }),
+    },
+  );
 }
