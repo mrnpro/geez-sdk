3f1e3dfe2839d85e09d2caeec34cab1063e3b66e
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2025-03-10 10:25:43 +0300

    fix: issues with dart analyzer

--- test/geez_test.dart
+++ test/geez_test.dart
@@ -9,22 +9,22 @@ void main() {
 
   group(
     'Geez Conversion Tests',
-    () => {
+    () {
       test('Convert a number to Geez - Specific Number #1', () {
         // Convert a specific number to Geez representation
         String geezRepresentation = 134.toGeez();
         expect(geezRepresentation, '፻፴፬');
-      }),
+      });
       test('Convert a number to Geez - Specific Number #2', () {
         // Convert a specific number to Geez representation
         String geezRepresentation = 1000001.toGeez();
         expect(geezRepresentation, '፻፼፩');
-      }),
+      });
       test('Convert a number to Geez - Specific Number #3', () {
         // Convert a specific number to Geez representation
         String geezRepresentation = 19876.toGeez();
         expect(geezRepresentation, '፼፺፰፻፸፮');
-      }),
+      });
       test('Convert a number to Geez - Multiple Numbers', () {
         // Convert multiple numbers to Geez representation
         for (var i = 0; i < numbersList.length; i++) {
@@ -32,29 +32,29 @@ void main() {
 
           expect(convertedToGeez, mockGeezNumbers[i]);
         }
-      }),
+      });
       test('Convert Geez to Number - Specific Number #1', () {
         // Convert a specific Geez representation to number
         num number = '፻፴፬'.toArabic();
         expect(number, 134);
-      }),
+      });
       test('Convert Geez to Number - Specific Number #2', () {
         // Convert a specific Geez representation to number
         num number = '፻፼፩'.toArabic();
         expect(number, 1000001);
-      }),
+      });
       test('Convert Geez to Number - Specific Number #3', () {
         // Convert a specific Geez representation to number
         num number = '፼፺፰፻፸፮'.toArabic();
         expect(number, 19876);
-      }),
+      });
       test('Convert Geez to Number - Multiple Numbers', () {
         // Convert multiple Geez representations to numbers
         for (var i = 0; i < numbersList.length; i++) {
           num convertedFromGeez = mockGeezNumbers[i].toArabic();
           expect(convertedFromGeez, numbersList[i]);
         }
-      }),
+      });
     },
   );
 }
